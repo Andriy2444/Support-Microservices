5e0ace379114ae158164086a79be04a851136b74
Orest Osadec <oorest06@gmail.com>
2025-12-26 10:20:27 +0200

    Added Full Ticket Service

--- apps/tickets-service/src/tickets/tickets.controller.ts
+++ apps/tickets-service/src/tickets/tickets.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Param, Patch, Delete, ParseIntPipe } from '@nestjs/common';
 import { TicketsService } from './tickets.service';
+import { IsString, IsInt, IsNotEmpty, IsOptional, IsEnum } from 'class-validator';
 import {
   ApiOperation,
   ApiTags,
@@ -9,13 +10,50 @@ import {
 
 class CreateTicketDto {
   @ApiProperty({ example: 'Проблема з доступом' })
+  @IsString()
+  @IsNotEmpty()
   title: string;
 
   @ApiProperty({ example: 'Не можу зайти в кабінет' })
+  @IsString()
+  @IsNotEmpty()
   description: string;
 
-  @ApiProperty({ example: 1 })
+  @ApiProperty({ example: 1, description: 'ID користувача' })
+  @IsInt()
   userId: number;
+}
+
+class UpdateTicketDto {
+  // @ApiProperty({ example: 'Оновлена назва', required: false })
+  // @IsString()
+  // @IsOptional()
+  // title?: string;
+
+  // @ApiProperty({ example: 'Оновлений опис', required: false })
+  // @IsString()
+  // @IsOptional()
+  // description?: string;
+
+  @ApiProperty({ example: 'CLOSED', enum: ['OPEN', 'IN_PROGRESS', 'CLOSED'], required: false })
+  @IsOptional()
+  status?: any;
+}
+
+class CreateMessageDto {
+  @ApiProperty({ example: 1, description: 'User Id' })
+  @IsInt()
+  userId: number;
+
+  @ApiProperty({ example: 'Пр, чо ніц не паше йоу', description: 'Message text' })
+  @IsString()
+  @IsNotEmpty()
+  message: string;
+
+  @ApiProperty({ example: 'https://i.imgur.com/cVACPds.jpeg', required: false })
+  @IsString()
+  @IsOptional()
+  image?: string;
 }
 
 @ApiTags('Tickets')
@@ -24,10 +62,17 @@ export class TicketsController {
   constructor(private readonly ticketService: TicketsService) {}
 
   @Get()
-  @ApiOperation({ summary: 'Get all tickets' }) // Опис методу
+  @ApiOperation({ summary: 'Get all tickets' })
   @ApiResponse({ status: 200, description: 'Return all tickets' })
-  async getTickets() {
+  async getAllTickets() {
     return this.ticketService.getAllTickets();
+  }
+
+  @Get(':userId')
+  @ApiOperation({ summary: 'Get user tickets' })
+  @ApiResponse({ status: 200, description: 'Return user tickets' })
+  async getTickets(@Param('userId', ParseIntPipe) userId: number) {
+    return this.ticketService.getTickets(userId);
   }
 
   @Post()
@@ -36,4 +81,47 @@ export class TicketsController {
   async createTicket(@Body() createTicketDto: CreateTicketDto) {
     return this.ticketService.createTicket(createTicketDto);
   }
+
+  @Patch(':id')
+  @ApiOperation({ summary: 'Update ticket' })
+  @ApiResponse({ status: 200, description: 'Ticket updated' })
+  async patchTicket(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateTicketDto: UpdateTicketDto
+  ) {
+    return this.ticketService.patchTicket(id, updateTicketDto);
+  }
+
+  @Delete(':id')
+  @ApiOperation({ summary: 'Delete ticket' })
+  @ApiResponse({ status: 200, description: 'Ticket deleted' })
+  async deleteTicket(@Param('id', ParseIntPipe) id: number) {
+    return this.ticketService.deleteTicket(id);
+  }
+
+  @Post(':id/users/:userId')
+  @ApiOperation({ summary: 'Add user to ticket' })
+  async addUserToTicket(
+    @Param('id', ParseIntPipe) id: number,
+    @Param('userId', ParseIntPipe) userId: number,
+  ) {
+    return this.ticketService.addUserToTicket(id, userId);
+  }
+
+  @Post(':ticketId/messages')
+  @ApiOperation({ summary: 'Send Message' })
+  @ApiResponse({ status: 201, description: 'Message sent' })
+  async createMessage(
+    @Param('ticketId', ParseIntPipe) ticketId: number,
+    @Body() createMessageDto: CreateMessageDto,
+  ) {
+    return this.ticketService.addMessage(ticketId, createMessageDto);
+  }
+
+  @Get()
+  @ApiOperation({ summary: 'Get all ticket messages' })
+  async getMessages(@Param('ticketId', ParseIntPipe) ticketId: number) {
+    return this.ticketService.getTicketMessages(ticketId);
+  }
+  
 }
--- apps/tickets-service/src/tickets/tickets.service.ts
+++ apps/tickets-service/src/tickets/tickets.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { Prisma } from '../generated/client'; // Імпортуйте типи з вашого клієнта
+import { Prisma } from '../generated/client';
 
 @Injectable()
 export class TicketsService {
@@ -10,14 +10,105 @@ export class TicketsService {
     return this.prisma.ticket.findMany();
   }
 
+  async getTickets( userId: number) {
+    const user = await this.prisma.user.findUnique({
+      where: { id: userId },
+      select: { role: true },
+    });
+
+    if (!user) {
+      throw new NotFoundException(`Користувача з ID ${userId} не знайдено`);
+    }
+
+    const roleId = user.role;
+    let whereClause: Prisma.TicketWhereInput = {};
+
+    if (roleId === 1) {
+      whereClause.users = {
+        some: { userId: userId },
+      };
+
+    } else if (roleId === 2) {
+      whereClause = {
+        OR: [
+          { status: 'OPEN' }, 
+          {
+            AND: [
+              { users: { some: { userId } } },
+              { status: { in: ['IN_PROGRESS', 'CLOSED'] } },
+            ],
+          },
+        ],
+      };
+
+    } else if (roleId === 3) {}
+
+    return this.prisma.ticket.findMany({
+      where: whereClause,
+      include: {
+        users: true,
+      },
+    });
+  }
+
   async createTicket(data: { title: string; description: string; userId: number }) {
     return this.prisma.ticket.create({
       data: {
         title: data.title,
         description: data.description,
-        userId: data.userId,
-        status: 'OPEN', // Значення за замовчуванням (або можна передати з data)
+        status: 'OPEN',
+        users: {
+          create: {
+            userId: data.userId,
+          },
+        },
       },
     });
   }
+
+  async patchTicket(id: number, data: Prisma.TicketUpdateInput) {
+    return this.prisma.ticket.update({
+      where: { id },
+      data,
+    });
+  }
+
+  async deleteTicket(id: number) {
+    return this.prisma.ticket.delete({
+      where: { id },
+    });
+  }
+
+  async addUserToTicket(ticketId: number, userId: number) {
+    return this.prisma.ticket.update({
+      where: { id: ticketId },
+      data: {
+        users: {
+          create: {
+            userId: userId,
+          },
+        },
+      },
+      include: { users: true }
+    });
+  }
+
+  async addMessage(ticketId: number, data: any) {
+    return this.prisma.message.create({
+      data: {
+        message: data.message,
+        image: data.image,
+        userId: data.userId,
+        ticketId: ticketId,
+      },
+    });
+  }
+
+  async getTicketMessages(ticketId: number) {
+    return this.prisma.message.findMany({
+      where: { ticketId },
+      orderBy: { createdAt: 'asc' },
+    });
+  }
+
 }
